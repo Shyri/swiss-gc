19d57f725adf9ee4e612e2a7b4d34ad279d7ac28
Shyri Villar <igs.villar@gmail.com>
2020-03-15 19:40:52 +0100

    Add exi sending patch

---
+++ cube/swiss/include/savestate.h
@@ -0,0 +1,8 @@
+//
+// Created by Shyri on 2020-03-15.
+//
+
+#ifndef SWISS_GC_SAVESTATE_H
+#define SWISS_GC_SAVESTATE_H
+
+#endif //SWISS_GC_SAVESTATE_H
---
+++ cube/swiss/source/savestate/savestate.c
@@ -0,0 +1,4 @@
+//
+// Created by Shyri on 2020-03-15.
+//
+
